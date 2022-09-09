ecf452b1875f2a5e06bd9e0e7f9383f2001b46f3
NUB31 <oliverlhs@outlook.com>
2022-09-09 16:49:49 +0200

    started work on api

--- express/api/src/server.ts
+++ express/api/src/server.ts
@@ -1,13 +1,16 @@
 // imports
-import cookieParser from "cookie-parser";
+
+import express, { Request, Response } from "express";
+import path, { dirname } from "path";
+
+import { Server } from "socket.io";
 import cors from "cors";
 import dotenv from "dotenv";
-import express, { Request, Response } from "express";
+import { exec } from "child_process";
+import { fileURLToPath } from "url";
 import fs from "fs/promises";
+import http from "http";
 import multer from "multer";
-import authenticateToken from "./middleware/authenticateUser.js";
-import authRoute from "./routes/auth.js";
-import userRoute from "./routes/user.js";
 
 dotenv.config();
 
@@ -17,7 +20,12 @@ const port = process.env.PORT || 3002;
 // All uploaded files will go to this path
 const upload = multer({ dest: "./upload/" });
 
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
 const app = express();
+const server = http.createServer(app);
+const io = new Server(server);
 
 // You cannot use * origin when including credentials
 app.use(
@@ -27,7 +35,7 @@ app.use(
   })
 );
 // Get access to jwt cookie
-app.use(cookieParser());
+// app.use(cookieParser());
 // For parsing different types of requests, and set limit high to prevent error
 app.use(
   express.json({
@@ -37,20 +45,43 @@ app.use(
 
 // Main API routes
 // Router routes
-app.use("/auth", authRoute);
-app.use("/user", userRoute);
+// app.use("/auth", authRoute);
+// app.use("/user", userRoute);
 // Set /upload endpoint to point to upload folder
-app.use("/upload", express.static("upload"));
+// app.use("/upload", express.static("upload"));
+app.use("/files", express.static("files"));
+
+io.on("connection", (socket) => {
+  console.log("a user connected");
+});
+
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "/static/index.html"));
+});
 
 // Upload single file
 app.post(
   "/upload",
-  authenticateToken,
   upload.single("file"),
   async (req: Request, res: Response) => {
     if (!req.file) return res.status(400).json("No file found in request");
+
     let newFileName = req.file.filename + req.file.originalname;
+
     await fs.rename(`./upload/${req.file.filename}`, `./upload/${newFileName}`);
+
+    exec("ls -la", (error, stdout, stderr) => {
+      if (error) {
+        console.log(`error: ${error.message}`);
+        return;
+      }
+      if (stderr) {
+        console.log(`stderr: ${stderr}`);
+        return;
+      }
+      console.log(`stdout: ${stdout}`);
+    });
+
     res.status(201).json({
       location: `${process.env.API_PUBLIC_URL}/upload/${newFileName}`,
     });
@@ -58,4 +89,4 @@ app.post(
 );
 
 // Listens on ENV or default port
-app.listen(port, () => console.log(`Listening on port: ${port}!`));
+server.listen(port, () => console.log(`Listening on port: ${port}!`));
--- express/api/src/utility/sanitizeUser.ts
+++
@@ -1,5 +0,0 @@
-import { User } from "@prisma/client";
-
-export default function sanitizeUser(user: User) {
-  return { ...user, password: undefined };
-}
