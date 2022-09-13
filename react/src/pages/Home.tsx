a9125947c926c9aeb33143da0eaf0ff3e54d57f8
NUB31 <oliverlhs@outlook.com>
2022-09-13 17:01:15 +0200

    logs and beatiful

--- react/src/pages/Home.tsx
+++ react/src/pages/Home.tsx
@@ -2,7 +2,7 @@ import Main from "../components/Main";
 
 export default function Home() {
   return (
-    <main className="flex justify-center items-center h-full w-full md:py-8">
+    <main className="flex justify-center">
       <Main />
     </main>
   );
