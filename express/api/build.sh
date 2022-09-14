6c9f016309632abf374fdeedb804da3eb808a487
NUB31 <oliverlhs@outlook.com>
2022-09-14 09:46:57 +0200

    added

--- express/api/build.sh
+++ express/api/build.sh
@@ -1,2 +1,4 @@
-docker build -t nub31/vscode-vs-theme-converter:backend -f docker/Dockerfile . &&
+SCRIPTPATH=$(dirname "$SCRIPT")
+
+docker build -t nub31/vscode-vs-theme-converter:backend -f SCRIPTPATH/docker/Dockerfile SCRIPTPATH &&
 docker push nub31/vscode-vs-theme-converter:backend
